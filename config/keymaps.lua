70d13ca95869a7abc6c0dd2bec77d0269d6bcf32
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-08-31 23:15:58 -0400

    feat!: change keymap.

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.cmd([[
-nnoremap ,i :exec "normal i".nr2char(getchar())."\e"<CR>
-nnoremap ,a :exec "normal a".nr2char(getchar())."\e"<CR>
+nnoremap <localleader>i :exec "normal i".nr2char(getchar())."\e"<CR>
+nnoremap <localleader>a :exec "normal a".nr2char(getchar())."\e"<CR>
 ]])
