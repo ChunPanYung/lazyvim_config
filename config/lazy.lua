5f38ee371af1b5ab9ee9247047bc0fed3174c4af
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-11-24 21:13:19 -0500

    feat: add rest testing tools.

--- config/lazy.lua
+++ config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.util.rest" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
     { import = "plugins" },
