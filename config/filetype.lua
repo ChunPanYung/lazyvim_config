aba9500b8289358aba0854e15d25439177278d96
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-12-09 17:39:12 -0500

    feat: setup filetype for cs and groovy file.

---
+++ config/filetype.lua
@@ -0,0 +1,16 @@
+vim.filetype.add({
+  extension = {
+    gvy = "groovy",
+    gy = "groovy",
+    gsh = "groovy",
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
