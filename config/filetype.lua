707624161d2ff7db497ca8b799606f7e9773edf2
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-12-09 17:39:43 -0500

    feat: add groovy filetype.

--- config/filetype.lua
+++ config/filetype.lua
@@ -7,7 +7,7 @@ vim.filetype.add({
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cs" },
+  pattern = { "cs", "groovy" },
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
