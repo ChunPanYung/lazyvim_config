c0efb3232910115901ee9f3492d189844fb5d627
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-12-10 11:24:33 -0500

    refactor: re-arrange pluings list.

--- config/lazy.lua
+++ config/lazy.lua
@@ -23,9 +23,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.util.rest" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
+
+    { import = "lazyvim.plugins.extras.util.rest" },
 
     { import = "plugins" },
   },
