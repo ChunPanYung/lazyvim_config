ae3ad7ad934b35707ac231e8cc2d68332ef79953
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-12-10 11:05:03 -0500

    feat!: remove mini files plugins.

--- config/lazy.lua
+++ config/lazy.lua
@@ -19,8 +19,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.editor.mini-files" },
-
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
