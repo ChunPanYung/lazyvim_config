9723a4f36ff25c9f5f1231c9e949f97fb10d52f9
Shawn Volker <infinite4ever@fedora.local>
2025-05-14 09:26:50 -0400

    feat!: remove omnisharp and restore dot.

--- config/lazy.lua
+++ config/lazy.lua
@@ -24,12 +24,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.json" },
 
     { import = "lazyvim.plugins.extras.util.rest" },
+    { import = "lazyvim.plugins.extras.util.dot" },
 
     { import = "plugins" },
   },
