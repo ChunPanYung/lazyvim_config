f4cd4f6098f0a27bd2aa31bddd332d05c8f0c299
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2025-09-16 22:48:33 -0400

    feat: set all visible by default.

--- plugins/snacks.lua
+++ plugins/snacks.lua
@@ -3,8 +3,20 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true,
-        ignored = true,
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
       },
     }, -- end opts
   },
