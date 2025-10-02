a818b88fb229fd25a7aa750ebf422a84047a1e15
ChunPanYung <31672021+ChunPanYung@users.noreply.github.com>
2025-10-02 19:23:22 -0400

    feat: hurl file format, show hidden by default. (#28)
    * feat!: do not search ignored file.
    
    * style: format hurl file.
    
    * refactor: simplify key dict.

--- config/autocmds.lua
+++ config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cs", "groovy", "json", "java" },
+  pattern = { "cs", "groovy", "json", "java", "hurl" },
   callback = function()
     vim.bo.shiftwidth = 4
     vim.bo.tabstop = 4
