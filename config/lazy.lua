21548aa1ea3662de3a9ea038613acfc1e56c7678
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2025-11-24 17:01:54 -0500

    feat: ai plugins.

--- config/lazy.lua
+++ config/lazy.lua
@@ -28,9 +28,14 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.rust" },
 
+    -- DevOps
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.helm" },
+
+    -- AI
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
 
     { import = "plugins" },
   },
