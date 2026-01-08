154b53bfdcb018b6333d435fa551ae9dbd331970
ChunPanYung <31672021+ChunPanYung@users.noreply.github.com>
2026-01-08 17:25:22 -0500

    feat: file filetype for `ps1` and `md`, enable `typescript` by default (#30)
    * feat: add `ps1` and `md` file
    
    * feat: enable typescript by default

--- config/autocmds.lua
+++ config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cs", "groovy", "json", "java", "hurl" },
+  pattern = { "cs", "groovy", "json", "java", "hurl", "ps1", "md" },
   callback = function()
     vim.bo.shiftwidth = 4
     vim.bo.tabstop = 4
